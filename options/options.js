fa16cc54a0609fc53bb0c57c0c5b1b1507b62a23
Crowdbotics <team@crowdbotics.com>
2023-12-31 21:16:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bekobodapp-45236",
+  url: "https://bekobodapp-45236.botics.co",
+  api: "https://bekobodapp-45236.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
